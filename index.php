0d67b5a21bfcd6f4bbe62276d5e0328ee80b2508
ayaakecil <160454614+ayaakecil@users.noreply.github.com>
2024-02-19 14:10:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>SELAMAT DATANG DI PERPUSTAKAAN</h1>
+    <ul>
+        <li><a href="../CRUD_PERPUSTAKAAN/peminjaman/v_daftar_peminjaman.php">peminjaman</a></li>
+        <li><a href="../CRUD_PERPUSTAKAAN/anggota/v_daftar_anggota.php">anggota</a></li>
+        <li><a href="../CRUD_PERPUSTAKAAN/buku/v_daftar_buku.php">buku</a></li>
+    </ul>
+</body>
+</html>
